75ee4a8f2f85785e00528709c7a5adcc3619ef03
Cristina Lugo <cmlugoce@syr.edu>
2018-06-13 17:07:10 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
 	monopoly = {:railroads => {
 		  :pieces => 4,
 		  :names => {
-		    :reading_railroad => {}
+		    :reading_railroad => {},
 		    
 		  },
 		  :rent_in_dollars => {
